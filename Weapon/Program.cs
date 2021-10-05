0e72229f35a6946e1fc3d08cbbf66fadf769593a
MezemovRufetCode <rufatam@code.edu.az>
2021-10-05 10:55:37 +0400

    Add project files.

---
+++ Weapon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Weapon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rifle rf1 = new Rifle();
+            Console.WriteLine("Total ammo:");
+            int T_ammo = int.Parse(Console.ReadLine());
+            rf1.Ammo = T_ammo;
+            Console.WriteLine("Magazine capacity:");
+            int cap = int.Parse(Console.ReadLine());       
+            rf1.Magazine = cap;          
+
+            if (T_ammo >= cap) {
+                Console.WriteLine("Currently used magazine ammo count:");
+                int usedammo = int.Parse(Console.ReadLine());
+                rf1.UAmmo = usedammo;
+                rf1.Reload(); 
+            }     
+            rf1.Shot();
+        }
+    }
+}
---
+++ Weapon/Rifle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon
+{
+    class Rifle
+    {
+        public int Ammo;
+        public int UAmmo; 
+        private int _magazine;
+        public Rifle()
+        {
+
+        }
+        public Rifle(int magCap, int totalAmmo, int uAmmo)
+        {
+            _magazine = magCap;
+            Ammo = totalAmmo;
+            UAmmo = uAmmo;
+        }
+        public int Magazine {
+            get 
+            {
+                if (_magazine == 0)
+                {
+                    return -1;
+                }
+                return _magazine;
+            }
+            set
+            {
+                if(value<20 || value >40)
+                {
+                    Console.WriteLine("Magazine capacity maybe max 40,min 20");
+                    return;
+                }
+                _magazine = value;
+            } 
+        }
+  
+        public void Shot()
+        {
+            for (int i = 0; i < Ammo; i++)
+            {
+                if (i == Magazine && Magazine>UAmmo)
+                {
+                    Ammo -= Magazine - (Magazine - UAmmo);
+                }
+                if (Ammo < Magazine)
+                {
+                    Console.WriteLine("U don't have enought ammo.");
+                    break;
+                }
+            }
+           
+            Console.WriteLine($"Total ammo : {Ammo}");
+        }  
+        public void Reload()
+        {
+            if (Magazine >= UAmmo)
+            {
+                if (Magazine == UAmmo)
+                {
+                    Console.WriteLine("U should reload!");
+                }
+                else
+                    Console.WriteLine($"U have still {Magazine - UAmmo} ammo in magazine.");
+            }
+            else Console.WriteLine("Currently used magazine ammo can not be more than capacity!");
+        }
+    }
+}
